cab568ba6dc4ca29f58bc3231d16e185af275d48
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-03-21 22:59:29 +0200

    Outline structure

--- types/octonode/index.d.ts
+++ types/octonode/index.d.ts
@@ -2,3 +2,23 @@
 // Project: https://github.com/pksunkara/octonode#readme
 // Definitions by: Dima Parzhitsky <https://github.com/parzhitsky>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare const octonode: {
+    // auth: import('./auth');
+    // client: import('./client');
+    // gist: import('./gist');
+    // issue: import('./issue');
+    // label: import('./label');
+    // me: import('./me');
+    // milestone: import('./milestone');
+    // org: import('./org');
+    // pr: import('./pr');
+    // project: import('./project');
+    // release: import('./release');
+    // repo: import('./repo');
+    // search: import('./search');
+    // team: import('./team');
+    // user: import('./user');
+};
+
+export = octonode;
