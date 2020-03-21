c7f1721f50f4346534d02f829a227f3b5c714d8f
Dmitry Parzhitsky <parzhitsky@gmail.com>
2020-03-21 22:58:51 +0200

    Add bare files

---
+++ types/octonode/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for octonode 0.9
+// Project: https://github.com/pksunkara/octonode#readme
+// Definitions by: Dima Parzhitsky <https://github.com/parzhitsky>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
